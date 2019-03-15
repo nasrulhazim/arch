c377b69c1ddf6a80123059541ae9104da22b9795
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-03-15 19:10:59 +0800

    Added alertify

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .copy('node_modules/alertifyjs/build/css', 'public/vendor/alertifyjs/css')
+   .copy('node_modules/alertifyjs/build/alertify.min.js', 'public/vendor/alertifyjs/js');
