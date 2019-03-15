8971c2453219c745b5b0668dad4de6d05b69463a
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-03-15 19:33:58 +0800

    Added Alertify Asset Blade Component

---
+++ resources/views/components/assets/alertify.blade.php
@@ -0,0 +1,7 @@
+@push('styles')
+	<link rel="stylesheet" type="text/css" href="{{ alertifyCss() }}">
+@endpush
+
+@push('scripts')
+	<script type="text/javascript" src="{{ alertifyJs() }}"></script>
+@endpush
