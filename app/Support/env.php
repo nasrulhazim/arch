2be80ca4a2f8a03d4fd3f09e91c5f3c39669dac2
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-05-21 11:33:57 +0800

    Adde env helpers

---
+++ app/Support/env.php
@@ -0,0 +1,21 @@
+<?php 
+
+/*
+ * Check environment status
+ */
+if (! function_exists('isProduction')) {
+    function isProduction()
+    {
+        return 'production' == app()->environment();
+    }
+}
+
+/*
+ * Check environment status
+ */
+if (! function_exists('isTesting')) {
+    function isTesting()
+    {
+        return 'testing' == app()->environment();
+    }
+}
