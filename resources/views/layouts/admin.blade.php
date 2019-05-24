22b439c515f73c176f09125a7e263c710604eb0a
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-05-24 08:07:07 +0800

    Remove breadcrumb in admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,11 +2,4 @@
 
 @section('navigation')
     @include('layouts.partials.navigation-top')
-    @container
-        @row 
-            @col 
-              {{ Breadcrumbs::render() }}  
-            @endcol
-        @endrow
-    @endcontainer
 @endsection 
