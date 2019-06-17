49f0f7925537803e76ed02e638622181e0207c65
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-06-18 04:18:54 +0800

    Added Blade Service Provider

---
+++ app/Providers/BladeServiceProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class BladeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->bootBladeDirectives();
+    }
+
+
+    private function bootBladeDirectives()
+    {
+        /*
+         * Cards
+         */
+        Blade::component('components.cards.base', 'card');
+        Blade::component('components.cards.body', 'cardbody');
+        Blade::component('components.cards.footer', 'cardfooter');
+        Blade::include('components.cards.header', 'cardheader');
+
+        /*
+         * Buttons
+         */
+        Blade::include('components.buttons.base', 'buttonbase');
+        Blade::include('components.buttons.new', 'buttonnew');
+        Blade::include('components.buttons.edit', 'buttonedit');
+        Blade::include('components.buttons.destroy', 'buttondestroy');
+        Blade::include('components.buttons.link', 'buttonlink');
+        Blade::include('components.buttons.submit', 'buttonsubmit');
+
+        /*
+         * Misc.
+         */
+        Blade::include('components.misc.breadcrumb', 'breadcrumb');
+    }
+}
