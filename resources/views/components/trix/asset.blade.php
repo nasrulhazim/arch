253784bbfc1e27c78cabf82d2ad8abe32df957f2
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-06-29 20:22:19 +0800

    Style Trix Toolbar

--- resources/views/components/trix/asset.blade.php
+++ resources/views/components/trix/asset.blade.php
@@ -1,5 +1,19 @@
 @push('styles')
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/trix.css') }}">
+	<style type="text/css">
+		.trix-toolbar, .trix-button-group, .trix-button  {
+			border: none !important;
+			border-radius: 0 !important;
+		}
+		.trix-button {
+			border-radius: 5px !important;
+			margin-right: 0.1rem !important;
+			margin-left: 0.1rem !important;
+		}
+		.trix-button.trix-active {
+			background: rgba(31, 58, 105, 0.4) !important;
+		}
+	</style>
 @endpush
 
 @push('scripts')
