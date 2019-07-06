7001217109faa3ef5ab9326b97879271d9ee9713
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-07-07 00:04:23 +0800

    Update bin/init

--- bin/init.sh
+++ bin/init.sh
@@ -4,5 +4,7 @@ composer install
 cp .env.example .env
 php artisan key:generate
 php artisan storage:link
+php artisan reload:cache
+php artisan reload:db
 php artisan passport:install
 yarn install
