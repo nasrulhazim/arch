0c707dafc791bb8f90f422d5adfa709b9eaf9a6d
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-09-10 08:33:52 +0800

    Added make:contract

---
+++ app/Console/Commands/Make/ContractCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands\Make;
+
+use Illuminate\Console\GeneratorCommand;
+
+class ContractCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:contract';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new Contract';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Contract';
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param string $rootNamespace
+     *
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\Contracts';
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__ . '/../stubs/contract.stub';
+    }
+}
