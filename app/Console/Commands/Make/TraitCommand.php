39ae484ce930780c72bea2b182126dbe4d1f3867
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-09-10 08:31:44 +0800

    Added make:trait

---
+++ app/Console/Commands/Make/TraitCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands\Make;
+
+use Illuminate\Console\GeneratorCommand;
+
+class TraitCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:trait';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new trait';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Trait';
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param string $rootNamespace
+     *
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\Traits';
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__ . '/../stubs/trait.stub';
+    }
+}
