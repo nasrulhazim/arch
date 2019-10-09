a45305abd73bcac2f21bfbda30f7ecfa17fa53af
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-10-09 08:36:52 +0800

    Reset Bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -33,6 +33,11 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
+	window.$.ajax({
+	  beforeSend: function( request ) {
+	  	request.setRequestHeader("X-CSRF-TOKEN", token.content);
+	  }
+	});
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
